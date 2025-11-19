8c0302ca02175a431fa3c1f8140fb70884aee1db
Ali Philip <131696518+Thelostduelist@users.noreply.github.com>
2025-11-19 13:15:14 -0500

    Create prep.py

---
+++ prep/prep.py
@@ -0,0 +1,12 @@
+message = """ Hi John, 
+
+THis is Alex from Alex Inc Corperations
+
+Best regards . . . """
+
+course = "Python Programming"
+print(len(course))
+print(course[-1]) # Prints from the back 
+print(course[0:3]) # Prints from the range 
+print(course[0:])
+print(course[:3])

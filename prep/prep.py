db42c5563e16b18adedab5fa0fd772edd0beca6d
Ali Philip <131696518+Thelostduelist@users.noreply.github.com>
2026-01-14 09:35:51 -0500

    Update prep.py

--- prep/prep.py
+++ prep/prep.py
@@ -1,12 +1,8 @@
-message = """ Hi John, 
+import collections
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+class frechDeck:
+    ranks = [n for n in range(2,11) + list('JKQA')]
+    suits = 'spade heart club diamond'.split()
 
-THis is Alex from Alex Inc Corperations
-
-Best regards . . . """
-
-course = "Python Programming"
-print(len(course))
-print(course[-1]) # Prints from the back 
-print(course[0:3]) # Prints from the range 
-print(course[0:])
-print(course[:3])
+def __initiaize(self):
+    self.cards = [Card(rank,suit)]
